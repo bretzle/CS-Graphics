71d3e312ce7bab495c935c3cc232a16546401b83
John Bretz <johnfish218@gmail.com>
2020-03-24 11:41:09 -0500

    auto format

--- Lab 2/matrix.cpp
+++ Lab 2/matrix.cpp
@@ -3,8 +3,8 @@
 #include <cmath>
 
 // Parameterized constructor
-matrix::matrix(unsigned int rows, unsigned int cols):rows(rows),cols(cols) 
-{  
+matrix::matrix(unsigned int rows, unsigned int cols) : rows(rows), cols(cols)
+{
 	if (rows < 1 || cols < 1)
 	{
 		throw matrixException("p-constructor bad arguments");
@@ -16,33 +16,33 @@ matrix::matrix(unsigned int rows, unsigned int cols):rows(rows),cols(cols)
 }
 
 // Copy constructor
-matrix::matrix(const matrix& from):rows(from.rows),cols(from.cols)
+matrix::matrix(const matrix &from) : rows(from.rows), cols(from.cols)
 {
-	the_matrix = new double[rows*cols];
+	the_matrix = new double[rows * cols];
 	(*this) = from;
 }
 
 // Destructor
 matrix::~matrix()
 {
-	delete [] the_matrix;
+	delete[] the_matrix;
 }
 
 // Assignment operator
-matrix& matrix::operator=(const matrix& rhs)
+matrix &matrix::operator=(const matrix &rhs)
 {
 	if (this != &rhs)
 	{
-		if ((rows*cols) != (rhs.rows*rhs.cols))
+		if ((rows * cols) != (rhs.rows * rhs.cols))
 		{
-			delete [] the_matrix;
+			delete[] the_matrix;
 			the_matrix = new double[rhs.rows * rhs.cols];
 		}
 
 		rows = rhs.rows;
 		cols = rhs.cols;
 
-		for (int i = 0; i < (rows*cols); i++)
+		for (int i = 0; i < (rows * cols); i++)
 		{
 			the_matrix[i] = rhs.the_matrix[i];
 		}
@@ -63,18 +63,17 @@ matrix matrix::identity(unsigned int size)
 	return ret;
 }
 
-
 // Binary operations
-matrix matrix::operator+(const matrix& rhs) const
+matrix matrix::operator+(const matrix &rhs) const
 {
-	if (rows!=rhs.rows || cols!=rhs.cols)
+	if (rows != rhs.rows || cols != rhs.cols)
 	{
 		throw matrixException("Operator+ size mismatch");
 	}
-	
+
 	matrix retVal(rows, cols);
 
-	for (int i = 0; i < (rows*cols); i++)
+	for (int i = 0; i < (rows * cols); i++)
 	{
 		retVal.the_matrix[i] = the_matrix[i] + rhs.the_matrix[i];
 	}
@@ -82,25 +81,22 @@ matrix matrix::operator+(const matrix& rhs) const
 	return retVal;
 }
 
-
-matrix matrix::operator*(const matrix& rhs) const
+matrix matrix::operator*(const matrix &rhs) const
 {
 	if (cols != rhs.rows)
 	{
 		throw matrixException("Operator+ size mismatch");
 	}
-	
+
 	matrix retVal(rows, rhs.cols);
 
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < rhs.cols; j++)
 		{
-			the_matrix[i*rhs.cols + j] = 0.0; // todo
+			the_matrix[i * rhs.cols + j] = 0.0; // todo
 		}
-		
 	}
-	
 
 	return retVal;
 }
@@ -112,7 +108,6 @@ matrix matrix::operator*(const double scale) const
 	return retVal;
 }
 
-
 // Unary operations
 matrix matrix::operator~() const
 {
@@ -122,87 +117,81 @@ matrix matrix::operator~() const
 	{
 		for (int j = 0; j < cols; j++)
 		{
-			retVal.the_matrix[i*rows+j] = the_matrix[j*rows+i];
+			retVal.the_matrix[i * rows + j] = the_matrix[j * rows + i];
 		}
 	}
 
 	return retVal;
 }
-	
 
 void matrix::clear()
 {
-	for (int i=0; i<(rows*cols); i++)
+	for (int i = 0; i < (rows * cols); i++)
 	{
 		the_matrix[i] = 0.0;
 	}
 	return;
 }
 
-double* matrix::operator[](unsigned int row)
+double *matrix::operator[](unsigned int row)
 {
-	double* ret = the_matrix;
+	double *ret = the_matrix;
 
-	ret += row*cols;
+	ret += row * cols;
 
 	return ret;
 }
 
-double* matrix::operator[](unsigned int row) const
+double *matrix::operator[](unsigned int row) const
 {
-	double* ret = the_matrix;
+	double *ret = the_matrix;
 
-	ret += row*cols;
+	ret += row * cols;
 
 	return ret;
 }
 
-
-std::ostream& matrix::out(std::ostream& os) const
+std::ostream &matrix::out(std::ostream &os) const
 {
 	os << "[[ ";
 	for (int z = 0; z < cols; z++)
 	{
 		os << the_matrix[z] << " ";
 	}
-	
+
 	os << "]" << std::endl;
 
 	for (int i = 1; i < rows - 1; i++)
 	{
 		os << " [ ";
 		for (int z = 0; z < cols; z++)
-	{
-		os << the_matrix[i*rows + z] << " ";
-	}
+		{
+			os << the_matrix[i * rows + z] << " ";
+		}
 		os << "]" << std::endl;
 	}
 
 	os << " [ ";
 	for (int z = 0; z < cols; z++)
 	{
-		os << the_matrix[(rows-1)*rows + z] << " ";
+		os << the_matrix[(rows - 1) * rows + z] << " ";
 	}
 	os << "]]";
 
 	return os;
 }
 
-
-
 // Global insertion and operator
-std::ostream& operator<<(std::ostream& os, const matrix& rhs)
+std::ostream &operator<<(std::ostream &os, const matrix &rhs)
 {
 	rhs.out(os);
 	return os;
 }
 
 // Global scalar multiplication
-matrix operator*(const double scale, const matrix& rhs)
+matrix operator*(const double scale, const matrix &rhs)
 {
 	// stub
 	matrix retval(rhs);
 	return retval;
 }
-
-
