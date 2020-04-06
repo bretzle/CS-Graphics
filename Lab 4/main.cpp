7c7f1c2d4a6962c187a76ff9780e8d3a324ca8d4
John Bretz <johnfish218@gmail.com>
2020-04-06 16:43:32 -0500

    Initialize lab 4

---
+++ Lab 4/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+#include <sstream>
+
+using namespace std;
+
+/**
+ * Main entry point
+ */
+int main(int argc, char **argv)
+{
+	cout << "Lab 4" << endl;
+	return 0;
+}
