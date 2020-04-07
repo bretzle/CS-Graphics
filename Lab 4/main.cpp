bd783484f50cb258bcc4ad6658d9f5ec7e8073da
John Bretz <johnfish218@gmail.com>
2020-04-07 14:14:51 -0500

    refactor main

--- Lab 4/main.cpp
+++ Lab 4/main.cpp
@@ -18,13 +18,15 @@ int main(int argc, char **argv)
 {
 	GraphicsContext *gc = new X11Context(500, 500, GraphicsContext::BLACK);
 
-	Line s(0, 0, 500, 500, 0xFF0000);
-	Triangle t(50, 450, 450, 450, 250, 50, 0xFF);
+	Line s1 = Line(0, 0, 500, 500, 0xFF0000);
+	Line s2 = Line(50, 50, 500, 50, 0xFF0000);
+	Triangle t1 = Triangle(50, 450, 450, 450, 250, 50, 0xFF);
 
-	s.draw(gc);
-	t.draw(gc);
-
-	cout << s << t;
+	s1.draw(gc);
+	s2.draw(gc);
+	t1.draw(gc);
+	
+	cout << s1 << s2 << t1;
 
 	// wait a while
 	sleep(5);
