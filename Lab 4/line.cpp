190c00a082bb911fa7e1873a1287a528fb3f9c6d
John Bretz <johnfish218@gmail.com>
2020-04-07 11:57:30 -0500

    move line and shape to own files

---
+++ Lab 4/line.cpp
@@ -0,0 +1,41 @@
+#include <iomanip>
+#include "shape.h"
+
+Line::Line(double x0, double y0, double x1, double y1) : Shape(), end(4, 1)
+{
+    this->origin[0][0] = x0;
+    this->origin[1][0] = y0;
+    this->origin[2][0] = 0;
+    this->origin[3][0] = 1;
+
+    this->end[0][0] = x1;
+    this->end[1][0] = y1;
+    this->end[2][0] = 0;
+    this->end[3][0] = 1;
+}
+
+Line::~Line()
+{
+}
+
+void Line::draw()
+{
+    out(std::cout);
+}
+
+std::ostream &Line::out(std::ostream &os) const
+{
+    os << "Line" << std::endl;
+
+    Shape::out(os);
+
+    os << "\tEnd: ("
+       << end[0][0]
+       << ","
+       << end[1][0]
+       << ","
+       << end[2][0]
+       << ")" << std::endl;
+
+    return os;
+}
---
+++ Lab 4/shape.cpp
@@ -0,0 +1,33 @@
+#include <iomanip>
+#include "shape.h"
+
+Shape::Shape() : color(0), origin(4, 1) {}
+
+Shape::Shape(const Shape &from) : color(from.color), origin(from.origin) {}
+
+Shape::~Shape() {}
+
+Shape &Shape::operator=(const Shape &rhs)
+{
+    if (this != &rhs)
+    {
+        color = rhs.color;
+        origin = matrix(rhs.origin);
+    }
+    return *this;
+}
+
+std::ostream &Shape::out(std::ostream &os) const
+{
+    os << "\tColor: "
+       << "0x"
+       << std::uppercase << std::setfill('0') << std::setw(6) << std::hex
+       << color << std::endl;
+
+    os << "\tOrigin: ("
+       << origin[0][0] << ","
+       << origin[1][0] << ","
+       << origin[2][0] << ")" << std::endl;
+
+    return os;
+}
